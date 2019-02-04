5530d60cc94d655b68e3192183b8a01703baa5aa
KayleeTheMech <kaylee@posteo.de>
2019-02-04 14:32:25 +0100

    implemented some methods on the graph

--- Graph.go
+++ Graph.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"gonum.org/v1/gonum/graph"
+	"math"
+)
+
+type Graph struct {
+	myNodes []Node
+	myEdges []Edge
+}
+
+func (g Graph) Node(id int64) graph.Node {
+	return NewNode("empty", 0)
+}
+
+func (g Graph) Nodes() graph.Nodes {
+
+}
+
+func (g Graph) From(id int64) graph.Nodes {
+
+}
+
+func (g Graph) HasEdgeBetween(xid, yid int64) bool {
+	for _, edge := range g.myEdges {
+		if edge.From().ID() == xid && edge.To().ID() == yid {
+			return true
+		}
+	}
+	return false
+}
+
+func (g Graph) Edge(uid, vid int64) graph.WeightedEdge {
+	for _, edge := range g.myEdges {
+		if edge.From().ID() == uid && edge.To().ID() == vid {
+			return edge
+		}
+	}
+	return nil
+}
+
+func (g Graph) WeightedEdge(uid, vid int64) graph.WeightedEdge {
+	return g.Edge(uid, vid)
+}
+
+func (g Graph) Weight(xid, yid int64) (w float64, ok bool) {
+	edge := g.WeightedEdge(xid, yid)
+	if edge != nil {
+		return edge.Weight(), true
+	}
+	return math.Inf(1), false
+}
--- NodeAndEdge.go
+++ NodeAndEdge.go
@@ -1,6 +1,47 @@
 package main
 
+import "gonum.org/v1/gonum/graph"
+
 type Node struct {
 	NodeName string
-	ad       int64
+	id       int64
+}
+
+func NewNode(name string, identity int64) graph.Node {
+	return Node{
+		NodeName: name,
+		id:       identity,
+	}
+}
+
+func (n Node) ID() int64 {
+	return n.id
+}
+
+type Edge struct {
+	EdgeName string
+	weight   float64
+	from     graph.Node
+	to       graph.Node
+}
+
+func NewEdge(name string, edgeWeight float64, fromNode graph.Node, toNode graph.Node) graph.WeightedEdge {
+	return Edge{
+		EdgeName: name,
+		weight:   edgeWeight,
+		from:     fromNode,
+		to:       toNode,
+	}
+}
+
+func (e Edge) From() graph.Node {
+	return e.from
+}
+
+func (e Edge) To() graph.Node {
+	return e.to
+}
+
+func (e Edge) Weight() float64 {
+	return e.weight
 }
